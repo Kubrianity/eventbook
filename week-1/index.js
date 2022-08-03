e26114c41da1eddd57672d5578cd63e8cd0129c7
Kubrianity <kubraaozerenn@gmail.com>
2022-08-03 16:53:05 +0300

    Update connect method to specify contacts' details

--- week-1/index.js
+++ week-1/index.js
@@ -10,7 +10,7 @@ class Person {
     }
     connect(person) {
         console.log(`Other ${this.upcomingEvent.attendees.length - 1} people are interested in this event, too! Connect with them!`);
-        this.contacts.push(person);
+        this.contacts.push({contactName : person.name, contactInfo : person.email});
     }
     comment(text , ratings) {
         let comment = new Comment(text, ratings);
@@ -57,6 +57,8 @@ kubra.comment('Yay! am so excited to see this', 5);
 console.log(`Kubra, you have ${kubra.contacts.length} contacts!`);
 
 upcomingEvent.news();
+
+
 
 
 
