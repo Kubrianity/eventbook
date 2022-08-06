815a996b1ba460c91075270ad69587845da746e9
Kubrianity <kubraaozerenn@gmail.com>
2022-08-06 13:27:04 +0300

    Add static method to NewEvent class

--- week-2/event.js
+++ week-2/event.js
@@ -1,11 +1,20 @@
+const Person = require('./person');
+
 module.exports = class NewEvent {
-    constructor(name, place, date) {
+    constructor(name, place, date, attendees = []) {
         this.name = name;
         this.place = place;
         this.date = date;
-        this.attendees = [];
+        this.attendees = attendees;
     }
     announce() {
         console.log(`${this.attendees.length} people are joining this event!`);
+    }
+    static create({name, place, date, attendees}) {
+        const newevent = new NewEvent(name, place, date, attendees);
+        if(newevent.attendees.length > 0) {
+           newevent.attendees = attendees.map(Person.create);
+        }
+        return newevent;
     }
 }
