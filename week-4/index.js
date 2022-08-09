cf334b9e6c969d5c8f56f174d014e87fcdeaa31a
Kubrianity <kubraaozerenn@gmail.com>
2022-08-09 23:06:50 +0300

    Get a specific person,event and comment with route parameter

--- week-4/index.js
+++ week-4/index.js
@@ -4,13 +4,13 @@ const PersonService = require('./services/person-service')
 const EventService = require('./services/event-service')
 const ReviewService = require( './services/review-service' )
 
-
 const app = express()
 app.set('view engine', 'pug')
 
 app.get('/', (req,res) => {
   res.render('index')
 })
+
 // Get all people
 app.get('/person/all', async (req,res) => {
   const people = await PersonService.findAll()
@@ -25,6 +25,25 @@ app.get('/events/all', async (req, res) => {
 app.get('/comments/all', async (req, res) => {
   const comments = await ReviewService.findAll()
   res.render('comment', { comments })
+})
+
+// Get a specific person
+app.get('/person/:id', async (req, res) => {
+  const id = req.params.id
+  const personDetail = await PersonService.find(id)
+  res.send(personDetail)
+})
+// Get a specific event
+app.get('/events/:id', async (req, res) => {
+  const id = req.params.id
+  const eventDetail = await EventService.find(id)
+  res.send(eventDetail)
+})
+// Get a specific comment
+app.get('/comments/:id', async (req, res) => {
+  const id = req.params.id
+  const comment = await ReviewService.find(id)
+  res.send(comment)
 })
 
 app.listen(3000, () => {
