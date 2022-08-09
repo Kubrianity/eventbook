9f98699a0eff90c59dc0e2d8a627b065e1f1532e
Kubrianity <kubraaozerenn@gmail.com>
2022-08-09 21:03:32 +0300

    Add URL paths to get all events and comments

--- week-4/index.js
+++ week-4/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
-// Get person service
+
 const PersonService = require('./services/person-service')
+const EventService = require('./services/event-service')
+const ReviewService = require( './services/review-service' )
+
 
 const app = express()
 app.set('view engine', 'pug')
@@ -8,10 +11,20 @@ app.set('view engine', 'pug')
 app.get('/', (req,res) => {
   res.render('index')
 })
-
+// Get all people
 app.get('/person/all', async (req,res) => {
   const people = await PersonService.findAll()
   res.render('person', { people })
+})
+// Get all events
+app.get('/events/all', async (req, res) => {
+  const events = await EventService.findAll()
+  res.render('event', { events })
+})
+// Get all comments
+app.get('/comments/all', async (req, res) => {
+  const comments = await ReviewService.findAll()
+  res.render('comment', { comments })
 })
 
 app.listen(3000, () => {
