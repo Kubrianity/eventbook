de725428c5d40df5eb07824e8eed5ea9bfdaf69d
Kubrianity <kubraaozerenn@gmail.com>
2022-08-09 18:53:58 +0300

    Set pug as view engine and render index file

--- week-4/index.js
+++ week-4/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 
 const app = express()
+app.set('view engine', 'pug')
+
+app.get('/', (req,res) => {
+  res.render('index')
+})
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000')
