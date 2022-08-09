1bec0e41525ab5d2f73ba249e5a0d8ec4d7131db
Kubrianity <kubraaozerenn@gmail.com>
2022-08-09 19:53:55 +0300

    Add URL path to render all people in the view file

--- week-4/index.js
+++ week-4/index.js
@@ -1,10 +1,17 @@
 const express = require('express')
+// Get person service
+const PersonService = require('./services/person-service')
 
 const app = express()
 app.set('view engine', 'pug')
 
 app.get('/', (req,res) => {
   res.render('index')
+})
+
+app.get('/person/all', async (req,res) => {
+  const people = await PersonService.findAll()
+  res.render('person', { people })
 })
 
 app.listen(3000, () => {
