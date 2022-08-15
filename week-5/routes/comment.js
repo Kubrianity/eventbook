6507a5fe05ca2c57bffc3153ca89703744c3cd3c
Kubrianity <kubraaozerenn@gmail.com>
2022-08-15 14:27:12 +0300

    Update the file for changes in model name

--- week-5/routes/comment.js
+++ week-5/routes/comment.js
@@ -1,25 +1,25 @@
 const express = require('express')
 const router = express.Router()
 
-const ReviewService = require('../services/review-service')
+const CommentService = require('../services/comment-service')
 
 router.get('/all', async (req, res) => {
-  const reviews = await ReviewService.findAll()
+  const reviews = await CommentService.findAll()
   res.render('reviews', { reviews })
 })
 
 router.get('/:id', async (req, res) => {
-  const review = await ReviewService.find(req.params.id)
+  const review = await CommentService.find(req.params.id)
   res.send(review)
 })
 
 router.post('/', async (req, res) => {
-  const newReview = await ReviewService.add(req.body)
+  const newReview = await CommentService.add(req.body)
   res.send(newReview)
 })
 
 router.delete('/:id', async (req, res) => {
-  await ReviewService.del(req.params.id)
+  await CommentService.del(req.params.id)
   res.send('Deleted!')
 })
 
