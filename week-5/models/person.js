83b70ce3ff159736bf251b61b50a79fdb0424482
Kubrianity <kubraaozerenn@gmail.com>
2022-08-16 13:47:01 +0300

    Move methods into person-service

--- week-5/models/person.js
+++ week-5/models/person.js
@@ -39,40 +39,6 @@ const PersonSchema = new mongoose.Schema({
     }]
 })
 
-// Create an event
-PersonSchema.methods.create = async function(event) {
-    this.createdEvents.push(event)
-    event.organizers.push(this)
-    await this.save()
-    await event.save()
-}
-
-// Register for an event
-PersonSchema.methods.register = async function(event) {
-    this.upcomingEvents.push(event)
-    event.attendees.push(this)
-    await this.save()
-    await event.save()
-}
-
-// Connect with a person
-PersonSchema.methods.connect = async function(person) {
-    this.contacts.push(person)
-    person.contacts.push(this)
-    await this.save()
-    await person.save()
-}
-
-// Make a comment about an event
-PersonSchema.methods.makeComment = async function(comment, event) {
-    this.comments.push(comment)
-    comment.author = this.name
-    event.comments.push(comment)
-    await this.save()
-    await comment.save()
-    await event.save()
-}
-
 PersonSchema.plugin(require('mongoose-autopopulate'))
 
 const PersonModel = mongoose.model('Person', PersonSchema)
