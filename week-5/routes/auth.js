04c35e8263acf3507574c550a6ab9cebd9b3c285
Kubrianity <kubraaozerenn@gmail.com>
2022-08-20 18:45:47 +0300

    Update redirect path

--- week-5/routes/auth.js
+++ week-5/routes/auth.js
@@ -11,16 +11,23 @@ router.get('/register', (req, res, next) => {
 router.get('/login', (req, res, next) => {
     res.render('login')
 })
+
 router.post('/register', (req, res, next) => {
     const user = new PersonModel({username : req.body.username})
     PersonModel.register( user, req.body.password, (err, account) => {
         if(err) return
         PersonService.add(account)
-        res.redirect('/')
+        res.redirect(`/person/${account._id}`)
     })
 })
-router.post('/login', passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/auth/login'
-}))
+
+router.post('/login',
+  passport.authenticate('local', {
+    failureRedirect: '/auth/login',
+    failureMessage: true 
+  }),
+  (req, res) => { // when authentication succeeds, the req.user property is set to the authenticated user
+    res.redirect(`/person/${req.user._id}`);
+});
+
 module.exports = router
