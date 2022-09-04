38d0574220ce4f9060640dda0870d917bf97d937
Kubrianity <kubraaozerenn@gmail.com>
2022-09-04 22:28:59 +0300

    Create event-card component

--- week-7/frontend/src/components/event-card.vue
+++ week-7/frontend/src/components/event-card.vue
@@ -1,32 +1,17 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+<template lang = 'pug'>
+div.name {{ event.name }} {{ event.place }} {{ event.date }}
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'event-card',
+  props: ['event']
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.name {
+  color: blue
 }
 </style>
