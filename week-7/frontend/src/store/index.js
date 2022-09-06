30c276c2f0597e65c9d416bcb1d33785a429d81b
Kubrianity <kubraaozerenn@gmail.com>
2022-09-07 00:03:03 +0300

    Add states, actions, mutations to fetch and auth

--- week-7/frontend/src/store/index.js
+++ week-7/frontend/src/store/index.js
@@ -1,21 +1,47 @@
 import { createStore } from 'vuex'
 import axios from 'axios'
+import router from '../router'
+
 export default createStore({
   state: {
-    events: []
+    events: [],
+    users: [],
+    user: {},
   },
   getters: {
   },
   mutations: {
     SET_EVENTS(state, data) {
       state.events = data
+    },
+    SET_USERS(state, data){
+      state.users = data
+    },
+    SET_USER(state, data) {
+      state.user = data
     }
-  },
+  }, 
   actions: {
     async fetchEvents({ commit }) {
       const result = await axios.get('http://localhost:3000/events/all/json')
       commit('SET_EVENTS', result.data)
-    }
+    },
+    async fetchUsers({ commit }) {
+      const result = await axios.get('http://localhost:3000/person/all/json')
+      commit('SET_USERS', result.data)
+    },
+    async logIn({commit}, user) {
+      const result = await axios.post(`http://localhost:3000/auth/login`, user, { withCredentials: true, origin: 'http://localhost:8080'})
+      if(!result.data) return
+      commit('SET_USER', result.data)
+      router.push('/user/profile')
+    },
+    async register({commit}, user) {
+      const result = await axios.post(`http://localhost:3000/auth/register`, user, { withCredentials: true, origin: 'http://localhost:8080'})
+      console.log(result)
+      commit('SET_USER', result.data)
+      router.push('/user/profile')
+    },   
   },
   modules: {
   }
