2d32f6e0743fe70cbece1ae836180bd3b23155f4
Kubrianity <kubraaozerenn@gmail.com>
2022-09-07 00:25:50 +0300

    Create users, user profile views

---
+++ week-7/frontend/src/views/UserProfile.vue
@@ -0,0 +1,24 @@
+<template lang = 'pug'>
+main
+  h1 Your Profile Page
+  h3 Hello, {{ user.username }}
+  
+</template>
+
+<script>
+// @ is an alias to /src
+import { mapState } from 'vuex'
+
+export default {
+  name: 'UserProfile',
+  computed: {
+    ...mapState(['user'])
+  },
+  methods: {
+
+  },
+  created() {
+  
+  }
+}
+</script>
---
+++ week-7/frontend/src/views/Users.vue
@@ -0,0 +1,28 @@
+<template lang = 'pug'>
+main
+  h1 Main Page
+  user-card(v-for = "user in users" :user="user" )
+  
+</template>
+
+<script>
+// @ is an alias to /src
+import UserCard from '@/components/user-card.vue'
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'Users',
+  components: {
+    UserCard
+  },
+  computed: {
+    ...mapState(['users'])
+  },
+  methods: {
+    ...mapActions(['fetchUsers'])
+  },
+  created() {
+    this.fetchUsers()
+  }
+}
+</script>
