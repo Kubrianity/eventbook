f6e96500b8b52f339ffcec3078f73e3d5b2f74d5
Kubrianity <kubraaozerenn@gmail.com>
2022-09-07 20:43:34 +0300

    Remove users router link

--- week-7/frontend/src/App.vue
+++ week-7/frontend/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/users">Users</router-link> |
     <router-link to="/register">Register</router-link> |
     <router-link to="/login">Login</router-link>
   </nav>
