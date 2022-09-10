bd142c8859d64c549bca03803a905f2ac8201d79
Kubrianity <kubraaozerenn@gmail.com>
2022-09-10 16:30:14 +0300

    Add router link to event form

--- week-7/frontend/src/views/UserProfile.vue
+++ week-7/frontend/src/views/UserProfile.vue
@@ -3,15 +3,17 @@ div.columns.is-mobile
   div.column
     div.box
       h3 Hello, {{ user.username }} 
-      div(v-if="user.upcomingEvents && user.upcomingEvents.length > 0")
+      div(v-if="user.upcomingEvents && user.upcomingEvents.length > 0") Upcoming events
         li(v-for="event in user.upcomingEvents", :key="event._id") {{ event.name }}
           button.button.is-primary(@click.prevent = 'handleClick(event._id)' type="button" value="Delete") Delete
       span(v-else) You have no upcoming events yet
 
-      div(v-if="user.createdEvents && user.createdEvents.length > 0")
+      div(v-if="user.createdEvents && user.createdEvents.length > 0") Organized events
         li(v-for="event in user.createdEvents", :key="event._id") {{ event.name }}
           button.button.is-primary(@click.prevent = 'handleClick(event._id)' type="button" value="Delete") Delete
       span(v-else) You have no organized events yet
+
+      <router-link to="/event-form">Create one</router-link>
 </template>
 
 <script>
