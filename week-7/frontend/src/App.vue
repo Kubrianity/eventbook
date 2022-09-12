f8b631a0b905be7ce1cc3fef03a0656f3d61c284
Kubrianity <kubraaozerenn@gmail.com>
2022-09-12 16:35:04 +0300

    Insert pug template and style elements with bulma

--- week-7/frontend/src/App.vue
+++ week-7/frontend/src/App.vue
@@ -1,31 +1,22 @@
-<template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/register">Register</router-link> |
-    <router-link to="/login">Login</router-link>
-  </nav>
-  <router-view/>
+<template lang = "pug">
+nav.navbar(role="navigation" aria-label="main navigation")
+  div.navbar-menu(id="navbarBasicExample")
+    div.navbar-start
+      a.navbar-item EVENTBOOK
+      router-link.navbar-item(to="/") Home
+    div.navbar-end
+      div.navbar-item
+        div.buttons
+            router-link.button.is-primary(to="/register")
+              strong Sign up
+            router-link.button.is-light(to="/login") Log in
+<router-view/>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+   text-align: center;
 }
 </style>
