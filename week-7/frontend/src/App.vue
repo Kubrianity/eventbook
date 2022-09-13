2bb27c20c78a1794faee64a81f35984996d1fd99
Kubrianity <kubraaozerenn@gmail.com>
2022-09-13 23:27:07 +0300

    Update style classes and add router link

--- week-7/frontend/src/App.vue
+++ week-7/frontend/src/App.vue
@@ -1,16 +1,18 @@
 <template lang = "pug">
-nav.navbar(role="navigation" aria-label="main navigation")
-  div.navbar-menu(id="navbarBasicExample")
+nav.navbar.is-dark(role="navigation" aria-label="main navigation")
+  div.navbar-menu(id="navbarExampleTransparentExample")
     div.navbar-start
       a.navbar-item EVENTBOOK
       router-link.navbar-item(to="/") Home
+      router-link.navbar-item(to="/user/profile") My Profile
     div.navbar-end
       div.navbar-item
         div.buttons
             router-link.button.is-primary(to="/register")
               strong Sign up
             router-link.button.is-light(to="/login") Log in
-<router-view/>
+<router-view/>            
+
 </template>
 
 <style>
