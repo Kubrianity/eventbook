9feb9c0d1c4fbea3f5c1341c1203be76fa22e851
Kubrianity <kubraaozerenn@gmail.com>
2022-09-18 18:09:01 +0300

    Update form style

--- week-7/frontend/src/components/event-creation-form.vue
+++ week-7/frontend/src/components/event-creation-form.vue
@@ -1,20 +1,22 @@
 <template lang = 'pug'>
-div.hero.is-fullheight
-  h2 Submit your event here
-    div.hero-body.is-justify-content-center.is-align-items-center
-      div.columns.is-flex.is-flex-direction-column.box
-        form(@submit.prevent = "handleFormRegister")
-          div.column
-            label Choose a name
-            input.input.is-primary(type="text" name="name" v-model="name") 
-          div.column
-            label Choose a location
-            input.input.is-primary(type="text" name="place"  v-model="place")
-          div.column
-            label Choose a date 
-            input.input.is-primary(type="date" name="date"  v-model="date") 
-          div.column
-            button.button.is-primary.is-fullwidth(type="submit") Submit
+h1.title.is-3(v-if="!user.username") You are not logged in yet
+
+div.hero.is-fullheight(v-else)
+  div.hero-body.is-justify-content-center.is-align-items-center
+    div.box
+      h1.title.is-3 Submit an event
+      form(@submit.prevent = "handleFormRegister")
+        div.column
+          label Choose a name
+          input.input.is-rounded(type="text" name="name" v-model="name" required) 
+        div.column
+          label Choose a location
+          input.input.is-rounded(type="text" name="place"  v-model="place" required)
+        div.column
+          label Choose a date 
+          input.input.is-rounded(type="date" name="date"  v-model="date" required) 
+        div.column
+          button.button.is-primary.is-fullwidth(type="submit") Submit
    
 </template>
 
