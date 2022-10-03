d8ca49255bf247e155e94db2b15f874c72f7f2b5
Kubrianity <kubraaozerenn@gmail.com>
2022-10-04 00:31:50 +0300

    Install and display spinner when user is redirected to homepage, fix spaces

--- week-7/frontend/src/App.vue
+++ week-7/frontend/src/App.vue
@@ -1,41 +1,59 @@
 <template lang = "pug">
-nav.navbar.is-spaced.is-dark(role="navigation" aria-label="main navigation")
+nav.navbar.is-spaced.is-dark(role = "navigation" aria-label = "main navigation")
   div.navbar-brand
-    a.navbar-burger(role="button" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample")
-      span(aria-hidden="true")
-      span(aria-hidden="true")
-      span(aria-hidden="true")
+    a.navbar-burger(role = "button" aria-label = "menu" aria-expanded = "false" data-target = "navbarBasicExample")
+      span(aria-hidden = "true")
+      span(aria-hidden = "true")
+      span(aria-hidden = "true")
 
-  div.navbar-menu(id="navbarBasicExample") 
+  div.navbar-menu(id = "navbarBasicExample") 
     div.navbar-start
       router-link.navbar-item(to = "/") HOME
-      router-link.navbar-item(to = "/user/profile" v-show="user.username") My Profile
-      router-link.navbar-item(to = "/event-form" v-show="user.username") Create an event
+      router-link.navbar-item(to = "/user/profile" v-show = "isAuthenticated") My Profile
+      router-link.navbar-item(to = "/event-form" v-show = "isAuthenticated") Create an event
     div.navbar-end
       div.navbar-item
         div.buttons
-            button.button.is-primary(@click = "logOut" v-show = "isAuthenticated")
+            button.button.is-primary(@click = "handleLogOut" v-show = "isAuthenticated")
               strong Log out
             router-link.button.is-primary(to = "/register" v-show = "!isAuthenticated")
               strong Sign up
             router-link.button.is-light(to = "/login" v-show = "!isAuthenticated") Log in
 
 router-view   
-
+loading(v-model:active = "isLoading" loader = "dots")
 </template>
 
 <script>
 
 import { mapState, mapActions, mapGetters } from 'vuex'
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      isLoading: false
+    }
+  },
+  components: {
+    Loading
+  },
   computed: {
     ...mapState(['user']),
     ...mapGetters(['isAuthenticated'])
   },
   methods: {
     ...mapActions(['logOut']),
+    handleLogOut() {
+      this.isLoading = true
+      setTimeout(() => {
+        this.isLoading = false
+        this.$router.push('/')
+        this.logOut()
+      }, 1000)
+    }
   },
   mounted() {
     // Toggle is-active class on touch devices
