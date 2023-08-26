8e80b7f78a8f41f0b8d28826728af1b2a5c9400e
Kubrianity <kubraaozerenn@gmail.com>
2023-08-26 23:38:41 +0300

    Update app.js

--- week-8/backend/app.js
+++ week-8/backend/app.js
@@ -1,15 +1,14 @@
 const express = require('express')
-require('dotenv').config()
 require('./database-connection')
-
-
+require('dotenv').config()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
-const eventRouter = require('./routes/event')
-const personRouter = require('./routes/person')
-const commentRouter = require('./routes/comment')
-const authRouter = require('./routes/auth')
+const app = express()
+app.use(cors({
+  credentials: true,
+  origin: true
+}))
 
 const PersonModel = require('./models/person')
 
@@ -18,11 +17,7 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const session = require('express-session')
 
-const app = express()
-
-app.use(cors())
-
-passport.serializeUser(PersonModel.serializeUser()) //these should come before initialize and session
+passport.serializeUser(PersonModel.serializeUser()) 
 passport.deserializeUser(PersonModel.deserializeUser())
 
 app.use(bodyParser.json())
@@ -36,12 +31,17 @@ app.use(cookieParser())
 app.use(bodyParser.json())
 app.set('view engine', 'pug')
 
+passport.use(new LocalStrategy(PersonModel.authenticate()))
+
+const eventRouter = require('./routes/event')
+const personRouter = require('./routes/person')
+const commentRouter = require('./routes/comment')
+const authRouter = require('./routes/auth')
+
 app.use('/events', eventRouter)
 app.use('/person', personRouter)
 app.use('/comments', commentRouter)
 app.use('/auth', authRouter)
-
-passport.use(new LocalStrategy(PersonModel.authenticate()))
 
 app.get('/', (req, res) => {
   res.render('index')
