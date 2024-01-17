40a6e6245d2959163e6d68657d24e32b166f6c37
Kubrianity <kubraaozerenn@gmail.com>
2024-01-17 18:47:36 +0300

    Update mongoose config

--- week-8/backend/database-connection.js
+++ week-8/backend/database-connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 async function main () {
+    mongoose.set('strictQuery', false)
     mongoose.connect(process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost/eventbook', {
         useNewUrlParser: true,
         useUnifiedTopology: true
